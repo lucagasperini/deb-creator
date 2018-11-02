460b751eff7501d9a12c44128e680e8c34d28fad
Luca Gasperini <git@xsoftware.eu>
2018-11-02 12:05:32 +0100

    Improve database code
    Signed-off-by: Luca Gasperini <git@xsoftware.eu>

--- lib/database.cpp
+++ lib/database.cpp
@@ -36,19 +36,19 @@ bool database::pkg_insert(const package *pkg)
         else
                 query->prepare(DB_PACKAGE_UPDATE);
 
-        query->bindValue(PKG_NAME, pkg->m_name);
-        query->bindValue(PKG_MAINTAINER, pkg->m_maintainer);
-        query->bindValue(PKG_UPLOADER, pkg->m_uploaders);
-        query->bindValue(PKG_VERSION, pkg->m_version);
-        query->bindValue(PKG_HOMEPAGE, pkg->m_homepage);
-        query->bindValue(PKG_SOURCE, pkg->m_source);
-        query->bindValue(PKG_ARCH, pkg->m_arch);
-        query->bindValue(PKG_DEPEND, pkg->m_depends);
-        query->bindValue(PKG_BUILD_DEP, pkg->m_build_dep);
-        query->bindValue(PKG_REPLACE, pkg->m_replace);
-        query->bindValue(PKG_SECTION, pkg->m_section);
-        query->bindValue(PKG_DESC_TITLE, pkg->m_desc_title);
-        query->bindValue(PKG_DESC_BODY, pkg->m_desc_body);
+        query->bindValue(PKG_NAME, QVariant(pkg->m_name));
+        query->bindValue(PKG_MAINTAINER, QVariant(pkg->m_maintainer));
+        query->bindValue(PKG_UPLOADER, QVariant(pkg->m_uploaders));
+        query->bindValue(PKG_VERSION, QVariant(pkg->m_version));
+        query->bindValue(PKG_HOMEPAGE, QVariant(pkg->m_homepage));
+        query->bindValue(PKG_SOURCE, QVariant(pkg->m_source));
+        query->bindValue(PKG_ARCH, QVariant(pkg->m_arch));
+        query->bindValue(PKG_DEPEND, QVariant(pkg->m_depends));
+        query->bindValue(PKG_BUILD_DEP, QVariant(pkg->m_build_dep));
+        query->bindValue(PKG_REPLACE, QVariant(pkg->m_replace));
+        query->bindValue(PKG_SECTION, QVariant(pkg->m_section));
+        query->bindValue(PKG_DESC_TITLE, QVariant(pkg->m_desc_title));
+        query->bindValue(PKG_DESC_BODY, QVariant(pkg->m_desc_body));
 
         bool offset = query->exec();
 
@@ -88,7 +88,7 @@ package *database::pkg_fetch(const QString &pkg)
         QSqlQuery* query = new QSqlQuery(*m_db);
 
         query->prepare(QSL("SELECT * FROM package WHERE name = :name"));
-        query->bindValue(QSL(":name"), pkg);
+        query->bindValue(PKG_NAME, QVariant(pkg));
 
         if(!query->exec()) {
 #ifdef QT_DEBUG
@@ -101,18 +101,18 @@ package *database::pkg_fetch(const QString &pkg)
 
         offset->m_name = pkg;
         while (query->next()) {
-                offset->m_maintainer = query->value(query->record().indexOf(QSL("maintainer"))).toString();
-                offset->m_uploaders = query->value(query->record().indexOf(QSL("uploader"))).toString();
-                offset->m_version = query->value(query->record().indexOf(QSL("version"))).toString();
-                offset->m_homepage = query->value(query->record().indexOf(QSL("homepage"))).toString();
-                offset->m_source = query->value(query->record().indexOf(QSL("source"))).toString();
-                offset->m_arch = (arch_t)query->value(query->record().indexOf(QSL("arch"))).toInt();
-                offset->m_depends = query->value(query->record().indexOf(QSL("depend"))).toString();
-                offset->m_build_dep = query->value(query->record().indexOf(QSL("build_dep"))).toString();
-                offset->m_replace = query->value(query->record().indexOf(QSL("replace"))).toString();
-                offset->m_section = query->value(query->record().indexOf(QSL("section"))).toString();
-                offset->m_desc_title = query->value(query->record().indexOf(QSL("title"))).toString();
-                offset->m_desc_body = query->value(query->record().indexOf(QSL("body"))).toString();
+                offset->m_maintainer = query->value(QSL("maintainer")).toString();
+                offset->m_uploaders = query->value(QSL("uploader")).toString();
+                offset->m_version = query->value(QSL("version")).toString();
+                offset->m_homepage = query->value(QSL("homepage")).toString();
+                offset->m_source = query->value(QSL("source")).toString();
+                offset->m_arch = (arch_t)query->value(QSL("arch")).toInt();
+                offset->m_depends = query->value(QSL("depend")).toString();
+                offset->m_build_dep = query->value(QSL("build_dep")).toString();
+                offset->m_replace = query->value(QSL("replace")).toString();
+                offset->m_section = query->value(QSL("section")).toString();
+                offset->m_desc_title = query->value(QSL("title")).toString();
+                offset->m_desc_body = query->value(QSL("body")).toString();
         }
 
         query->finish();
@@ -124,7 +124,7 @@ bool database::pkg_exists(const QString &pkg)
         QSqlQuery* query = new QSqlQuery(*m_db);
 
         query->prepare(QSL("SELECT EXISTS(SELECT 1 FROM package WHERE name=:name)"));
-        query->bindValue(":name", pkg);
+        query->bindValue(PKG_NAME, QVariant(pkg));
 
         if(!query->exec()) {
 #ifdef QT_DEBUG
@@ -143,7 +143,7 @@ bool database::pkg_remove(const QString &pkg)
         QSqlQuery* query = new QSqlQuery(*m_db);
 
         query->prepare(QSL("DELETE FROM package WHERE name=:name"));
-        query->bindValue(":name", pkg);
+        query->bindValue(PKG_NAME, QVariant(pkg));
 
         if(!query->exec()) {
 #ifdef QT_DEBUG
@@ -169,10 +169,10 @@ bool database::build_insert(const QString &pkg, const build_step &step)
         else
                 query->prepare(DB_BUILD_UPDATE); /* FIXME: DIDN'T WORK */
 
-        query->bindValue(BUILD_PROGRAM, step.program);
-        query->bindValue(BUILD_ARG, step.argument);
-        query->bindValue(BUILD_DIR, step.directory);
-        query->bindValue(BUILD_PKG, pkg);
+        query->bindValue(BUILD_PROGRAM, QVariant(step.program));
+        query->bindValue(BUILD_ARG, QVariant(step.argument));
+        query->bindValue(BUILD_DIR, QVariant(step.directory));
+        query->bindValue(BUILD_PKG, QVariant(pkg));
 
         bool offset = query->exec();
 
@@ -206,9 +206,9 @@ QList<build_step*>* database::build_fetch(const QString &pkg)
 
         while (query->next()) {
                 buffer = new build_step;
-                buffer->program = query->value(query->record().indexOf(QSL("program"))).toString();
-                buffer->argument = query->value(query->record().indexOf(QSL("argument"))).toString();
-                buffer->directory = query->value(query->record().indexOf(QSL("directory"))).toString();
+                buffer->program = query->value(QSL("program")).toString();
+                buffer->argument = query->value(QSL("argument")).toString();
+                buffer->directory = query->value(QSL("directory")).toString();
                 offset->append(buffer);
         }
 
@@ -221,10 +221,10 @@ bool database::build_exists(const QString &pkg, const build_step &step)
         QSqlQuery* query = new QSqlQuery(*m_db);
 
         query->prepare(QSL("SELECT EXISTS(SELECT 1 FROM build WHERE program=:program AND argument=:argument AND directory=:directory AND pkg=:pkg)"));
-        query->bindValue(BUILD_PROGRAM, step.program);
-        query->bindValue(BUILD_ARG, step.argument);
-        query->bindValue(BUILD_DIR, step.directory);
-        query->bindValue(BUILD_PKG, pkg);
+        query->bindValue(BUILD_PROGRAM, QVariant(step.program));
+        query->bindValue(BUILD_ARG, QVariant(step.argument));
+        query->bindValue(BUILD_DIR, QVariant(step.directory));
+        query->bindValue(BUILD_PKG, QVariant(pkg));
 
         if(!query->exec()) {
 #ifdef QT_DEBUG
@@ -243,10 +243,10 @@ bool database::build_remove(const QString &pkg, const build_step &step)
         QSqlQuery* query = new QSqlQuery(*m_db);
 
         query->prepare(QSL("DELETE FROM build WHERE program=:program AND argument=:argument AND directory=:directory AND pkg=:pkg"));
-        query->bindValue(BUILD_PROGRAM, step.program);
-        query->bindValue(BUILD_ARG, step.argument);
-        query->bindValue(BUILD_DIR, step.directory);
-        query->bindValue(BUILD_PKG, pkg);
+        query->bindValue(BUILD_PROGRAM, QVariant(step.program));
+        query->bindValue(BUILD_ARG, QVariant(step.argument));
+        query->bindValue(BUILD_DIR, QVariant(step.directory));
+        query->bindValue(BUILD_PKG, QVariant(pkg));
 
         if(!query->exec()) {
 #ifdef QT_DEBUG
